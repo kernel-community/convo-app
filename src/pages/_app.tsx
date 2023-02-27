21bf4d57132cee72921b66fbd7e63b07c65f8135
Angela Gilhotra <angelagilhotra@gmail.com>
2023-02-27 18:31:28 +0530

    add seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,6 +13,7 @@ import type { Session } from "next-auth";
 import type { GetSiweMessageOptions } from "@rainbow-me/rainbowkit-siwe-next-auth";
 import { RainbowKitSiweNextAuthProvider } from "@rainbow-me/rainbowkit-siwe-next-auth";
 import { Analytics } from "@vercel/analytics/react";
+import { NextSeo } from "next-seo";
 
 const queryClient = new QueryClient();
 const { chains, provider } = configureChains(
@@ -40,6 +41,70 @@ const MyApp = ({
 }>) => {
   return (
     <>
+      <NextSeo
+        titleTemplate="Convo | %s"
+        defaultTitle="KERNEL Convo"
+        description="A home for conversations taking place in the Kernel network"
+        openGraph={{
+          url: "https://convo.kernel.community",
+          title: "KERNEL Conversations",
+          description:
+            "A home for conversations taking place in the Kernel network",
+          images: [
+            {
+              url: "https://confab-frontend.vercel.app/images/banner.jpg",
+              alt: "KERNEL squares and circles",
+              type: "image/jpeg",
+            },
+          ],
+          site_name: "KERNEL Convo",
+        }}
+        twitter={{
+          handle: "@kernel0x",
+          site: "https://kernel.community",
+          cardType: "summary_large_image",
+        }}
+        additionalLinkTags={[
+          {
+            rel: "stylesheet",
+            href: "https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap",
+          },
+          {
+            rel: "stylesheet",
+            href: "https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@200;300;400;500;600;700;800;900&display=swap",
+          },
+          {
+            rel: "stylesheet",
+            href: "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap",
+          },
+          {
+            rel: "stylesheet",
+            href: "https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap",
+          },
+          {
+            rel: "stylesheet",
+            href: "https://fonts.googleapis.com/css2?family=Alegreya+Sans+SC:ital,wght@0,300;0,400;0,500;0,700;0,800;1,300;1,400;1,500;1,700;1,800&display=swap",
+          },
+          {
+            rel: "stylesheet",
+            href: "https://fonts.googleapis.com/css2?family=Nanum+Myeongjo:wght@400;700;800&display=swap",
+          },
+          {
+            rel: "stylesheet",
+            href: "https://fonts.googleapis.com/css2?family=Licorice&display=swap",
+          },
+          {
+            rel: "stylesheet",
+            href: "https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap",
+          },
+          {
+            rel: "preload",
+            href: "/fonts/Futura/futura-medium.ttf",
+            as: "font",
+            crossOrigin: "anonymous",
+          },
+        ]}
+      />
       <SessionProvider session={session}>
         <WagmiConfig client={wagmiClient}>
           <RainbowKitSiweNextAuthProvider
