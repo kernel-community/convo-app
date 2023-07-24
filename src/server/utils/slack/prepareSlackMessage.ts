0cabe98b506cf85950b63204b0367f28b6179c5f
Angela <angelagilhotra@gmail.com>
2023-07-24 15:21:30 -0700

    add notification types (#108)

--- src/server/utils/slack/prepareSlackMessage.ts
+++ src/server/utils/slack/prepareSlackMessage.ts
@@ -1,8 +1,5 @@
 import type { ServerEvent } from "src/types";
 
-const DEFAULT_USERNAME = "New convo scheduled";
-const DEFAULT_TEXT = "New Convo Alert";
-
 type SlackEventMessage = {
   blocks: SlackMessageBlocks[];
   icon?: string;
@@ -15,23 +12,49 @@ type SlackMessageBlocks = {
   text?: object;
 };
 
+type MessageType = "new" | "reminder" | "updated";
+
+const getUsernameAndText = (
+  type: MessageType
+): { text: string; icon: string } => {
+  switch (type) {
+    case "new": {
+      return { text: "New Convo", icon: ":speech_balloon:" };
+    }
+    case "reminder": {
+      return { text: "Reminder", icon: ":mantelpiece_clock:" };
+    }
+    case "updated": {
+      return { text: "Convo Updated", icon: ":new_moon_with_face:" };
+    }
+  }
+};
+
 export const prepareSlackMessage = ({
   event,
   reqHost,
+  type,
 }: {
   event: ServerEvent;
   reqHost: string;
+  type: MessageType;
 }): SlackEventMessage => {
   const { title, descriptionHtml, hash, proposer } = event;
   const protocol = reqHost.includes("localhost") ? "http" : "https";
   const url = `${protocol}://${reqHost}/rsvp/${hash}`;
+
+  const description =
+    `${descriptionHtml?.substring(0, 250)}` +
+    `${descriptionHtml && descriptionHtml?.length > 250 ? "..." : ""}`;
+
+  const { text, icon } = getUsernameAndText(type);
 
   const blocks: SlackMessageBlocks[] = [];
   blocks.push({
     type: "section",
     text: {
       type: "mrkdwn",
-      text: `*${proposer.nickname}*` + ` has proposed a new Convo!`,
+      text: `Convo by *${proposer.nickname}*`,
     },
   });
   blocks.push({
@@ -41,8 +64,11 @@ export const prepareSlackMessage = ({
     type: "section",
     text: {
       type: "mrkdwn",
-      text: `*${title}*\n${descriptionHtml}`,
+      text: `*${title}*\n${description}`,
     },
+  });
+  blocks.push({
+    type: "divider",
   });
   if (event.series) {
     blocks.push({
@@ -69,9 +95,13 @@ export const prepareSlackMessage = ({
       },
     ],
   });
+  blocks.push({
+    type: "divider",
+  });
   return {
     blocks,
-    username: DEFAULT_USERNAME,
-    text: DEFAULT_TEXT,
+    username: text,
+    text,
+    icon,
   };
 };
