5cff28cee2f502a3d87a8772e91860e74ec56652
Angela <angelagilhotra@gmail.com>
2023-07-30 20:08:55 -0700

    send gcal invite to the proposer (#115)

--- src/pages/api/actions/google/createEvent.ts
+++ src/pages/api/actions/google/createEvent.ts
@@ -11,6 +11,7 @@ import { pick } from "lodash";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { createEvents } from "src/server/utils/google/createEvent";
 import { prisma } from "src/server/db";
+import { sendInvite } from "src/server/utils/google/sendInvite";
 
 export type FullEvent = Event & {
   proposer: User;
@@ -25,7 +26,13 @@ export default async function createEventHandler(
     "host",
   ]);
 
-  const { events }: { events: Array<FullEvent> } = pick(req.body, ["events"]);
+  const {
+    events,
+    proposerEmail,
+  }: { events: Array<FullEvent>; proposerEmail: string } = pick(req.body, [
+    "events",
+    "proposerEmail",
+  ]);
 
   if (!events) {
     throw new Error("`event` not found in req.body");
@@ -64,6 +71,22 @@ export default async function createEventHandler(
   });
 
   const updated = await Promise.all(updatePromises);
+
+  // send calendar invites to the proposer
+  if (proposerEmail) {
+    const eventIds = updated.map((e) => e.id);
+    await sendInvite({
+      events: eventIds,
+      attendeeEmail: proposerEmail,
+    });
+    console.log(`
+      Sent google calendar invites to the proposer ${proposerEmail}
+    `);
+  }
+
+  console.log(`
+    Created google calendar events for ${JSON.stringify(updated)}
+  `);
 
   res.status(200).json({
     data: updated,
