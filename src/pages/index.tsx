901fa8961d382e66231d80b912d7573e0253e295
Angela Gilhotra <angelagilhotra@gmail.com>
2023-08-16 15:22:09 -0700

    new cta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Main from "src/layouts/Main";
 import Link from "next/link";
 import { Events } from "src/components/Events";
+import type { ReactNode } from "react";
 const Home: NextPage = () => {
   return (
     <>
@@ -13,36 +14,44 @@ const Home: NextPage = () => {
           lg:pl-64
         "
         >
+          <Link href="/all">
+            <div
+              className="
+                my-2
+                font-heading text-5xl
+                font-bold
+                lg:text-7xl
+              "
+            >
+              Convo
+            </div>
+          </Link>
           <div
             className="
-                pt-12
+                ml-2
+                pt-3
                 font-secondary
                 text-lg
                 text-kernel
               "
           >
-            Wander off in a&nbsp;
+            Why are we alive, if not to
+            <Link href="/propose">
+              <span className="cursor-pointer">
+                <FancyHighlight>
+                  <span className="underline decoration-dashed">
+                    make connections
+                  </span>
+                </FancyHighlight>
+                <span className="ml-1">?</span>
+              </span>
+            </Link>
           </div>
-          <Link href="/all">
-            <span className="relative mt-2 inline-block cursor-pointer before:absolute before:-inset-1 before:block before:-skew-y-3 before:bg-highlight">
-              <div
-                className="
-              relative
-              font-heading
-              text-5xl font-bold
-              text-primary
-              lg:text-7xl
-            "
-              >
-                Convo.
-              </div>
-            </span>
-          </Link>
         </div>
         <div className="mt-12 px-12 lg:px-32 xl:px-40 2xl:px-52">
-          <div className="mt-12">
+          {/* <div className="mt-12">
             <Events type="live" highlight="Live" take={6} />
-          </div>
+          </div> */}
           <div className="mt-12">
             <Events type="today" title="upcoming" highlight="today" take={6} />
           </div>
@@ -52,6 +61,14 @@ const Home: NextPage = () => {
         </div>
       </Main>
     </>
+  );
+};
+
+export const FancyHighlight = ({ children }: { children: ReactNode }) => {
+  return (
+    <span className="relative ml-2 inline-block cursor-pointer before:absolute before:-inset-1 before:block before:-skew-y-3 before:bg-highlight">
+      <span className="relative text-primary">{children}</span>
+    </span>
   );
 };
 export default Home;
