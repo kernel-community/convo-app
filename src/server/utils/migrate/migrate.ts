ead4ae6bc12aa8883af997a75c4ee1861a558dcb
Angela Gilhotra <angelagilhotra@gmail.com>
2023-08-16 17:32:28 -0700

    remove migration data

--- src/server/utils/migrate/migrate.ts
+++ src/server/utils/migrate/migrate.ts
@@ -1,64 +1,64 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { prisma } from "../../db";
-import { data } from "./data";
+// import { prisma } from "../../db";
+// import { data } from "./data";
 
 export const migrate = async () => {
-  const events = data.data;
-  let allCreated = [];
-  const allCreatedPromises = [];
-  for (let i = 0; i < events.length; i++) {
-    const {
-      id,
-      hash,
-      title,
-      descriptionHtml,
-      startDateTime,
-      endDateTime,
-      location,
-      limit,
-      proposerEmail,
-      proposer,
-      series,
-      GoogleCalendar,
-      RSVP,
-    } = events[i]!;
-    const gcal = GoogleCalendar[0]!;
-    allCreatedPromises.push(
-      prisma.event.upsert({
-        where: { id: id.toString() },
-        create: {
-          id: id.toString(),
-          title,
-          descriptionHtml,
-          startDateTime,
-          endDateTime,
-          location,
-          hash,
-          limit,
-          type: "JUNTO",
-          isImported: true,
-          proposer: {
-            connectOrCreate: {
-              where: {
-                address: proposerEmail,
-              },
-              create: {
-                nickname: proposer.username,
-                address: proposerEmail,
-              },
-            },
-          },
-          series,
-          gCalEventRequested: !!gcal,
-          gCalEventId: gcal ? gcal?.gCalEventId : undefined,
-          gCalId: gcal ? gcal?.gCalCalendarId : undefined,
-          emails: RSVP.map((r: { attendeeEmail: string }) => r.attendeeEmail),
-        },
-        update: {},
-        select: { id: true },
-      })
-    );
-  }
-  allCreated = await Promise.all(allCreatedPromises);
-  return allCreated;
+  // const events = data.data;
+  // let allCreated = [];
+  // const allCreatedPromises = [];
+  // for (let i = 0; i < events.length; i++) {
+  //   const {
+  //     id,
+  //     hash,
+  //     title,
+  //     descriptionHtml,
+  //     startDateTime,
+  //     endDateTime,
+  //     location,
+  //     limit,
+  //     proposerEmail,
+  //     proposer,
+  //     series,
+  //     GoogleCalendar,
+  //     RSVP,
+  //   } = events[i]!;
+  //   const gcal = GoogleCalendar[0]!;
+  //   allCreatedPromises.push(
+  //     prisma.event.upsert({
+  //       where: { id: id.toString() },
+  //       create: {
+  //         id: id.toString(),
+  //         title,
+  //         descriptionHtml,
+  //         startDateTime,
+  //         endDateTime,
+  //         location,
+  //         hash,
+  //         limit,
+  //         type: "JUNTO",
+  //         isImported: true,
+  //         proposer: {
+  //           connectOrCreate: {
+  //             where: {
+  //               address: proposerEmail,
+  //             },
+  //             create: {
+  //               nickname: proposer.username,
+  //               address: proposerEmail,
+  //             },
+  //           },
+  //         },
+  //         series,
+  //         gCalEventRequested: !!gcal,
+  //         gCalEventId: gcal ? gcal?.gCalEventId : undefined,
+  //         gCalId: gcal ? gcal?.gCalCalendarId : undefined,
+  //         emails: RSVP.map((r: { attendeeEmail: string }) => r.attendeeEmail),
+  //       },
+  //       update: {},
+  //       select: { id: true },
+  //     })
+  //   );
+  // }
+  // allCreated = await Promise.all(allCreatedPromises);
+  // return allCreated;
 };
