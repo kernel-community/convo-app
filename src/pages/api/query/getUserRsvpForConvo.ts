14da3d4ae4a1daf8fc45f2c1cb273a512122d345
Angela <angelagilhotra@gmail.com>
2024-02-05 16:13:52 -0800

    Ag/rsvp UI fix (#159)

---
+++ src/pages/api/query/getUserRsvpForConvo.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import _ from "lodash";
+import { prisma } from "src/server/db";
+
+const getUserRsvpForConvo = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  console.log("getUserRsvpForConvo");
+  const { userId, hash } = _.pick(req.body, ["userId", "hash"]);
+  if (!userId || !hash) {
+    throw new Error(`invalid request body: ${JSON.stringify(req.body)}`);
+  }
+
+  const rsvps = await prisma.rsvp.findMany({
+    where: {
+      attendeeId: userId,
+      event: { hash },
+    },
+  });
+
+  res.status(200).json({
+    data: {
+      rsvps,
+    },
+  });
+};
+
+export default getUserRsvpForConvo;
