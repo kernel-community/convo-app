80ba1bc384cbb0861ee9465e2f1eeb4312d507a6
Angela Gilhotra <angelagilhotra@gmail.com>
2024-02-20 22:43:34 -0800

    fix richtext component

--- src/components/ProposeForm/FormFields/RichText.tsx
+++ src/components/ProposeForm/FormFields/RichText.tsx
@@ -11,10 +11,34 @@ import BulletList from "@tiptap/extension-bullet-list";
 import OrderedList from "@tiptap/extension-ordered-list";
 import ListItem from "@tiptap/extension-list-item";
 import Placeholder from "@tiptap/extension-placeholder";
-import { useCallback } from "react";
+import type { ReactNode } from "react";
+import { Fragment, useCallback } from "react";
 import type { FieldErrorsImpl } from "react-hook-form";
 import type { ClientEventInput } from "..";
 import FieldLabel from "../../StrongText";
+import type { Editor } from "@tiptap/core";
+import Collaboration from "@tiptap/extension-collaboration";
+import * as Y from "yjs";
+import Highlight from "@tiptap/extension-highlight";
+import {
+  BoldIcon,
+  Code2,
+  CodeIcon,
+  Heading1Icon,
+  Heading2Icon,
+  Heading3Icon,
+  HighlighterIcon,
+  ItalicIcon,
+  LinkIcon,
+  ListIcon,
+  ListMinus,
+  ListMinusIcon,
+  ListOrderedIcon,
+  PilcrowIcon,
+  RedoIcon,
+  UndoIcon,
+} from "lucide-react";
+
 type handleChangeType = (e: any) => void;
 
 export const RichTextArea = ({
@@ -46,7 +70,7 @@ export const RichTextArea = ({
         openOnClick: false,
       }),
       Heading.configure({
-        levels: [2, 3],
+        levels: [1, 2, 3],
       }),
       BulletList,
       OrderedList,
@@ -55,41 +79,19 @@ export const RichTextArea = ({
         placeholder: "Write away!",
         showOnlyWhenEditable: true,
       }),
+      Collaboration.configure({
+        document: new Y.Doc(),
+      }),
+      Highlight,
     ],
     editorProps: {
       attributes: {
-        class: `prose focus:outline-none prose-stone leading-0.5`,
+        class: `prose focus:outline-none prose-stone`,
       },
     },
     onUpdate: ({ editor }) => handleChange(editor.getHTML()),
     content: value,
   });
-
-  const setLink = useCallback(() => {
-    const previousUrl = editor?.getAttributes("link").href;
-    const url = window.prompt("URL", previousUrl);
-
-    // cancelled
-    if (url === null) {
-      return;
-    }
-
-    // empty
-    if (url === "") {
-      editor?.chain().focus().extendMarkRange("link").unsetLink().run();
-
-      return;
-    }
-
-    // update link
-    editor
-      ?.chain()
-      .focus()
-      .extendMarkRange("link")
-      .setLink({ href: url })
-      .run();
-  }, [editor]);
-
   return (
     <div>
       {fieldName && (
@@ -101,82 +103,7 @@ export const RichTextArea = ({
         </FieldLabel>
       )}
       <div className="rounded-lg border-2 border-primary-muted p-3">
-        <div className="border-b-2 pb-2">
-          <button
-            onClick={() =>
-              editor?.chain().focus().toggleHeading({ level: 2 }).run()
-            }
-            className={
-              `${
-                editor?.isActive("heading", { level: 2 })
-                  ? "bg-primary text-primary-muted"
-                  : "bg-white text-primary"
-              }` +
-              ` ` +
-              `mx-1 rounded-lg border-2 border-gray-800 px-2 font-secondary text-sm uppercase`
-            }
-          >
-            h1
-          </button>
-          <button
-            onClick={() =>
-              editor?.chain().focus().toggleHeading({ level: 3 }).run()
-            }
-            className={
-              `${
-                editor?.isActive("heading", { level: 3 })
-                  ? "bg-primary text-primary-muted"
-                  : "bg-white text-primary"
-              }` +
-              ` ` +
-              `mx-1 rounded-lg border-2 border-gray-800 px-2 font-secondary text-sm uppercase`
-            }
-          >
-            h2
-          </button>
-          <button
-            onClick={() => editor?.chain().focus().toggleBold().run()}
-            className={
-              `${
-                editor?.isActive("bold")
-                  ? "bg-primary text-primary-muted"
-                  : "bg-white text-primary"
-              }` +
-              ` ` +
-              `mx-1 rounded-lg border-2 border-gray-800 px-2 font-secondary text-sm uppercase`
-            }
-          >
-            bold
-          </button>
-          <button
-            onClick={() => editor?.chain().focus().toggleItalic().run()}
-            className={
-              `${
-                editor?.isActive("italic")
-                  ? "bg-primary text-primary-muted"
-                  : "bg-white text-primary"
-              }` +
-              ` ` +
-              `mx-1 rounded-lg border-2 border-gray-800 px-2 font-secondary text-sm uppercase`
-            }
-          >
-            italic
-          </button>
-          <button
-            onClick={setLink}
-            className={
-              `${
-                editor?.isActive("link")
-                  ? "bg-primary text-primary-muted"
-                  : "bg-white text-primary"
-              }` +
-              ` ` +
-              `mx-1 rounded-lg border-2 border-gray-800 px-2 font-secondary text-sm uppercase`
-            }
-          >
-            set link
-          </button>
-        </div>
+        {editor && <MenuBar editor={editor} />}
         <EditorContent editor={editor} />
       </div>
       <div className="font-primary text-sm lowercase text-red-400">
@@ -188,3 +115,140 @@ export const RichTextArea = ({
     </div>
   );
 };
+
+const iconStyle = "w-5 h-5";
+export const MenuBar = ({ editor }: { editor: Editor }) => {
+  const items = [
+    {
+      icon: <BoldIcon className={`${iconStyle}`} />,
+      title: "Bold",
+      action: () => editor.chain().focus().toggleBold().run(),
+      isActive: () => editor.isActive("bold"),
+    },
+    {
+      icon: <ItalicIcon className={`${iconStyle}`} />,
+      title: "Italic",
+      action: () => editor.chain().focus().toggleItalic().run(),
+      isActive: () => editor.isActive("italic"),
+    },
+    {
+      icon: <CodeIcon className={`${iconStyle}`} />,
+      title: "Code",
+      action: () => editor.chain().focus().toggleCode().run(),
+      isActive: () => editor.isActive("code"),
+    },
+    // {
+    //   icon: <HighlighterIcon />,
+    //   title: "Highlight",
+    //   action: () => editor.chain().focus().toggleHighlight().run(),
+    //   isActive: () => editor.isActive("highlight"),
+    // },
+    {
+      icon: <Heading1Icon className={`${iconStyle}`} />,
+      title: "Heading 1",
+      action: () => editor.chain().focus().toggleHeading({ level: 1 }).run(),
+      isActive: () => editor.isActive("heading", { level: 1 }),
+    },
+    {
+      icon: <Heading2Icon className={`${iconStyle}`} />,
+      title: "Heading 2",
+      action: () => editor.chain().focus().toggleHeading({ level: 2 }).run(),
+      isActive: () => editor.isActive("heading", { level: 2 }),
+    },
+    {
+      icon: <Heading3Icon className={`${iconStyle}`} />,
+      title: "Heading 3",
+      action: () => editor.chain().focus().toggleHeading({ level: 3 }).run(),
+      isActive: () => editor.isActive("heading", { level: 3 }),
+    },
+    {
+      icon: <LinkIcon className={`${iconStyle}`} />,
+      title: "Link",
+      action: useCallback(() => {
+        const previousUrl = editor.getAttributes("link").href;
+        const url = window.prompt("URL", previousUrl);
+
+        // cancelled
+        if (url === null) {
+          return;
+        }
+
+        // empty
+        if (url === "") {
+          editor.chain().focus().extendMarkRange("link").unsetLink().run();
+
+          return;
+        }
+
+        // update link
+        editor
+          .chain()
+          .focus()
+          .extendMarkRange("link")
+          .setLink({ href: url })
+          .run();
+      }, [editor]),
+      isActive: () => editor.isActive("link"),
+    },
+    {
+      icon: <ListIcon className={`${iconStyle}`} />,
+      title: "Bullet List",
+      action: () => editor.chain().focus().toggleBulletList().run(),
+      isActive: () => editor.isActive("bulletList"),
+    },
+    {
+      icon: <ListOrderedIcon className={`${iconStyle}`} />,
+      title: "Ordered List",
+      action: () => editor.chain().focus().toggleOrderedList().run(),
+      isActive: () => editor.isActive("orderedList"),
+    },
+    {
+      icon: <UndoIcon className={`${iconStyle}`} />,
+      title: "Undo",
+      action: () => editor.chain().focus().undo().run(),
+    },
+    {
+      icon: <RedoIcon className={`${iconStyle}`} />,
+      title: "Redo",
+      action: () => editor.chain().focus().redo().run(),
+    },
+  ];
+
+  return (
+    <div className="flex flex-row flex-wrap items-center gap-3">
+      {items.map((item, index) => (
+        <Fragment key={index}>
+          <MenuItem {...item} />
+        </Fragment>
+      ))}
+    </div>
+  );
+};
+
+const MenuItem = ({
+  icon,
+  title,
+  action,
+  isActive = null,
+}: {
+  icon?: ReactNode;
+  title?: string;
+  action?: () => void;
+  isActive?: (() => boolean) | null;
+}) => (
+  <span
+    // className={`menu-item${isActive && isActive() ? " is-active" : ""}`}
+    className={
+      `${
+        isActive && isActive()
+          ? "bg-primary text-primary-muted"
+          : "bg-white text-primary"
+      }` +
+      ` inline-flex cursor-pointer flex-row items-center gap-2 rounded-lg border-2 border-gray-800 px-2 font-secondary text-xs uppercase`
+    }
+    onClick={action}
+    title={title}
+  >
+    {icon}
+  </span>
+);
