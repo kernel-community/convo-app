83151e5400701024831ab0c847e4617305ba62e7
Angela Gilhotra <angelagilhotra@gmail.com>
2024-03-03 12:45:49 -0800

    Add mobile nav using drawer

--- src/components/Navbar/Items.tsx
+++ src/components/Navbar/Items.tsx
@@ -3,10 +3,22 @@ import {
   CalendarDays,
   CalendarPlus,
   HomeIcon,
+  MoreHorizontal,
 } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { Fragment } from "react";
+import { useMediaQuery } from "src/hooks/useMediaQuery";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "src/components/ui/drawer";
 
 const iconStyle = "w-4 h-4";
 
@@ -32,30 +44,59 @@ const items = [
     href: "/propose",
   },
 ];
+const desktop = "(min-width: 768px)";
 
 export const Items = () => {
   const { pathname } = useRouter();
   const isActive = (path: string) => pathname === path;
+  const isDesktop = useMediaQuery(desktop);
+  if (isDesktop) {
+    return (
+      <div className="flex flex-row items-center gap-3">
+        {items.map((item, index) => (
+          <Fragment key={index}>
+            <Link href={item.href}>
+              <span
+                className={`group inline-flex cursor-pointer flex-row items-center gap-2 rounded-full bg-zinc-600/50 px-[0.7rem]
+                py-[0.3rem] text-sm font-light lowercase text-slate-400
+                    ${isActive(item.href) && "bg-zinc-600/80 !text-slate-200"}
+                  `}
+              >
+                {item.icon}
+                {!isActive(item.href) && (
+                  <span className="hidden group-hover:block">{item.text}</span>
+                )}
+                {isActive(item.href) && item.text}
+              </span>
+            </Link>
+          </Fragment>
+        ))}
+      </div>
+    );
+  }
   return (
-    <div className="flex flex-row items-center gap-3">
-      {items.map((item, index) => (
-        <Fragment key={index}>
-          <Link href={item.href}>
-            <span
-              className={`group inline-flex cursor-pointer flex-row items-center gap-2 rounded-full bg-zinc-600/50 px-[0.7rem]
-              py-[0.3rem] text-sm font-light lowercase text-slate-400
-                  ${isActive(item.href) && "bg-zinc-600/80 !text-slate-200"}
+    <Drawer direction="right">
+      <DrawerTrigger>
+        <MoreHorizontal />
+      </DrawerTrigger>
+      <DrawerContent className="left-24 h-full">
+        <div className="flex flex-col gap-3 p-3">
+          {items.map((item, index) => (
+            <Fragment key={index}>
+              <Link href={item.href}>
+                <span
+                  className={`group inline-flex cursor-pointer flex-row items-center gap-2 text-sm font-light lowercase text-black
+                ${isActive(item.href) ? `border-b-2 border-black` : ``}
                 `}
-            >
-              {item.icon}
-              {!isActive(item.href) && (
-                <span className="hidden group-hover:block">{item.text}</span>
-              )}
-              {isActive(item.href) && item.text}
-            </span>
-          </Link>
-        </Fragment>
-      ))}
-    </div>
+                >
+                  {item.icon}
+                  <span className="">{item.text}</span>
+                </span>
+              </Link>
+            </Fragment>
+          ))}
+        </div>
+      </DrawerContent>
+    </Drawer>
   );
 };
