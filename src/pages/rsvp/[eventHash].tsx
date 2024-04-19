06ee6b63f3483468e90209c4b533c59c60bd095a
Angela Gilhotra <angelagilhotra@gmail.com>
2024-04-19 15:58:55 -0700

    use suspense

--- src/pages/rsvp/[eventHash].tsx
+++ src/pages/rsvp/[eventHash].tsx
@@ -6,6 +6,7 @@ import Head from "next/head";
 import type { ClientEvent } from "src/types";
 import useEvent from "src/hooks/useEvent";
 import { useRouter } from "next/router";
+import { Suspense } from "react";
 
 const Post = ({ hostname, data }: { hostname: string; data: ClientEvent }) => {
   const { fetchedUser: user } = useUser();
@@ -22,53 +23,55 @@ const Post = ({ hostname, data }: { hostname: string; data: ClientEvent }) => {
     user && fetchedEventData ? user.id === fetchedEventData.proposerId : false;
 
   return (
-    <Main className="px-6 lg:px-52">
-      <Head>
-        <title>{`Convo | ${data.title}`}</title>
-        <meta name="description" content="The kernel of a conversation" />
-        {/* OpenGraph */}
-        <meta
-          property="og:title"
-          content={`Convo | ${data?.title}`}
-          key="title"
-        />
-        <meta
-          property="og:url"
-          content={
-            hostname.includes("localhost")
-              ? `http://${hostname}`
-              : `https://${hostname}`
-          }
-        />
-        <meta
-          property="og:image"
-          content="https://confab-frontend.vercel.app/images/banner.jpg"
-        />
-
-        {/* Twitter */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={`Convo | ${data?.title}`} />
-        <meta
-          name="twitter:image"
-          content="https://confab-frontend.vercel.app/images/banner.jpg"
-        />
-
-        <meta name="robots" content="index, follow, nocache" />
-        <meta
-          name="googlebot"
-          content="index, follow, noimageindex, max-video-preview:-1, max-image-preview:large, max-snippet:-1"
-        />
-      </Head>
-      <RsvpIntentionProvider>
-        {!isLoading && !isError && fetchedEventData && (
-          <EventWrapper
-            event={fetchedEventData}
-            isEditable={isEditable}
-            hostname={hostname}
+    <Suspense fallback={<p>Loading post...</p>}>
+      <Main className="px-6 lg:px-52">
+        <Head>
+          <title>{`Convo | ${data.title}`}</title>
+          <meta name="description" content="The kernel of a conversation" />
+          {/* OpenGraph */}
+          <meta
+            property="og:title"
+            content={`Convo | ${data?.title}`}
+            key="title"
           />
-        )}
-      </RsvpIntentionProvider>
-    </Main>
+          <meta
+            property="og:url"
+            content={
+              hostname.includes("localhost")
+                ? `http://${hostname}`
+                : `https://${hostname}`
+            }
+          />
+          <meta
+            property="og:image"
+            content="https://confab-frontend.vercel.app/images/banner.jpg"
+          />
+
+          {/* Twitter */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content={`Convo | ${data?.title}`} />
+          <meta
+            name="twitter:image"
+            content="https://confab-frontend.vercel.app/images/banner.jpg"
+          />
+
+          <meta name="robots" content="index, follow, nocache" />
+          <meta
+            name="googlebot"
+            content="index, follow, noimageindex, max-video-preview:-1, max-image-preview:large, max-snippet:-1"
+          />
+        </Head>
+        <RsvpIntentionProvider>
+          {!isLoading && !isError && fetchedEventData && (
+            <EventWrapper
+              event={fetchedEventData}
+              isEditable={isEditable}
+              hostname={hostname}
+            />
+          )}
+        </RsvpIntentionProvider>
+      </Main>
+    </Suspense>
   );
 };
 
@@ -89,6 +92,7 @@ const fetchEventData = async (hash: string, prefix: string) => {
   }
 };
 
+// i forgot why i needed server side rendering here 😐
 export async function getServerSideProps(context: any) {
   const hash = context.params["eventHash"];
   const host = context.req.headers.host;
@@ -96,7 +100,7 @@ export async function getServerSideProps(context: any) {
     ? `http://${host}`
     : `https://${host}`;
   const r = await fetchEventData(hash, prefix);
-  console.log(r);
+
   return {
     props: {
       // Access the host from the headers (consider the "x-forwarded-host" if behind a proxy)
