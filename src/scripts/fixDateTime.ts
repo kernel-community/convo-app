dc0e4831ae6d6c5030d8bc9e47b57c75f15ef863
Angela Gilhotra <angelagilhotra@gmail.com>
2024-07-16 13:08:43 -0700

    update var

--- src/scripts/fixDateTime.ts
+++ src/scripts/fixDateTime.ts
@@ -1,22 +1,22 @@
 import { DateTime } from "luxon";
 
 const convertTimeToZone = ({
-  dt,
+  inISO,
   sourceZone,
   targetZone,
 }: {
-  dt: string;
+  inISO: string;
   sourceZone: string;
   targetZone: string;
 }) => {
-  return DateTime.fromISO(dt, { zone: sourceZone })
+  return DateTime.fromISO(inISO, { zone: sourceZone })
     .setZone(targetZone)
     .toISO()
     ?.toString();
 };
 console.log(
   convertTimeToZone({
-    dt: "2024-08-07T16:30:00",
+    inISO: "2024-08-07T16:30:00",
     sourceZone: "America/New_York",
     targetZone: "utc",
   })
