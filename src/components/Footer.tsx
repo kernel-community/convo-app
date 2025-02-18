3c09ec0dc3ff7bece58248c9d7643f42d8b5042d
Angela Gilhotra <angelagilhotra@gmail.com>
2025-02-17 16:41:36 -0800

    Add frens page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,8 @@
 import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { Heart, MoveUpRight } from "lucide-react";
+import Link from "next/link";
+import { HeartEmoji } from "./ui/emojis";
 const Footer = () => {
   const [isMounted, setIsMounted] = useState(false);
 
@@ -18,14 +20,13 @@ const Footer = () => {
     >
       <div className="flex flex-row items-center justify-center gap-1 italic">
         Built{" "}
-        <a
-          href="https://github.com/Kernel-Community/convo-app"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="underline decoration-dotted  underline-offset-4"
+        <Link
+          href="/frens"
+          className="flex flex-row  items-center font-secondary underline decoration-dotted underline-offset-4"
         >
-          with love
-        </a>
+          with frens{" "}
+          <HeartEmoji width={33} height={33} className="-ml-1 -mr-2" />
+        </Link>
         <span>
           at{" "}
           <div className="relative inline-flex font-primary">
