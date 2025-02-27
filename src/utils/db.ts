28ace00025ed79363fc8e798d3f81f1a7f1293b5
Angela Gilhotra <angelagilhotra@gmail.com>
2025-02-27 00:26:25 -0800

    remove query and info logs

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -11,7 +11,7 @@ declare global {
 export const prisma =
   global.prisma ||
   new PrismaClient({
-    log: ["query", "info", "warn", "error"],
+    log: ["warn", "error"],
   }).$extends({
     query: {
       event: {
